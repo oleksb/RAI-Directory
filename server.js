a259c9d9464c67f360c69d852713ebcace2c3675
Superunknown1103 <superunknown1103@gmail.com>
2017-12-06 14:02:41 -0600

    server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+const PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+// =============================================================
+
+app.listen(PORT, () => {
+  console.log('Server started on PORT ' + PORT);
+})
