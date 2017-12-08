3aa4a0e726c8c995ef88fc5ebf0ce90a56d42b76
Superunknown1103 <superunknown1103@gmail.com>
2017-12-08 15:09:41 -0600

    basic functionality acheived

--- public/js/main.js
+++ public/js/main.js
@@ -13,15 +13,16 @@ $( document ).ready(function() {
         $(".weatherImg").html("<img height='100px' width='100px' src='" + iconUrl  + "'>");
         });
 
+        var page = $('#overlay');
         var currentTime = moment().format("hh:mm a");
         var currentDate = moment().format('dddd') + ', ' + moment().format("ll");
         $('.clock').empty();
         $('.clock').append(currentTime);
         $('.date').empty();
         $('.date').append(currentDate);
+        
 
 // Interval firing
-
     setInterval(function() {
         var currentTime = moment().format("hh:mm a");
         var currentDate = moment().format('dddd') + ', ' + moment().format("ll");
@@ -29,7 +30,19 @@ $( document ).ready(function() {
         $('.clock').append(currentTime);
         $('.date').empty();
         $('.date').append(currentDate);
-}, 60000 );
+        var screenCheck = moment().format('LT');
+        console.log(screenCheck);
+        if (screenCheck == '7:00 AM'){
+        jQuery(function($) {
+            $("#over").css('visibility', 'hidden');
+        });
+    };
+        if (screenCheck == '7:00 PM'){
+            jQuery(function($) {
+                $("#over").css('visibility', 'hidden');
+            });
+    };
+}, 5000 );
 
     setInterval(function() {
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?zip=60659,us&units=imperial&APPID=15c4449b72461cbf6e40e27d8225c3e2";
@@ -44,5 +57,5 @@ $( document ).ready(function() {
                 $(".weatherImg").html("<img height='100px' width='100px' src='" + iconUrl  + "'>");
         });
     }, 600000 );
+        
     })
-
